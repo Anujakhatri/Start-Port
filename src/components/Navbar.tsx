87adabd57411c37f9480cd681e40c694d03f852f
Anuja khatri <khatrianuja20@gmail.com>
2026-02-16 16:23:29 +0545

    feat: Initialize project structure by removing boilerplate code, creating Navbar and HomePage components, and updating global styles.

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+export function Navbar() {
+    return (
+        <>
+            <div className="header">
+                <li>Home </li>
+                <li>About </li>
+                <li>Projects </li>
+                <li>Contact </li>
+            </div>
+        </>
+    )
+}
---
+++ src/pages/Home/HomePage.tsx
@@ -0,0 +1,12 @@
+import { Navbar } from "../../components/Navbar"
+export function HomePage() {
+    return (
+        <>
+            <div className="home-container">
+                <div className="navbar">
+                    <Navbar />
+                </div>
+            </div>
+        </>
+    )
+}
